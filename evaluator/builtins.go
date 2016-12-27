2e0689fc8fd112fbbb7228c6a2712e6982a7199e
Upal <uks2sh@virginia.edu>
2016-12-27 14:36:52 -0500

    Implemented puts function, finished implementations in book

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey_interpreter/object"
+import (
+	"fmt"
+	"monkey_interpreter/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -101,6 +104,14 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 }
